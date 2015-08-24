14fa2945fc014f7a476b18709e485705cc244e7d
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-24 18:04:55 -0500

    Modified dancer to work with setTimeOut binding

--- src/dancer.js
+++ src/dancer.js
@@ -4,16 +4,22 @@ var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
 };
 
+var count = 1;
 Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  console.log('----scheduling----');
+  console.log(count);
+  count++;
+  console.log(this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
