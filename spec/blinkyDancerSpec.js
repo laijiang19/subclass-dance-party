3bb5119e64c1c346cf6833341511d77ef006b9f9
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-24 18:05:36 -0500

    Completed BlinkyDancerSpec class and updated tests. Passed all tests

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,16 +1,22 @@
 describe("blinkyDancer", function() {
 
   var blinkyDancer;
-  var timeBetweenSteps = 100;
+  var timeBetweenSteps = 10000;
   var clock;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
+    console.log(blinkyDancer);
   });
 
-  it("should have a jQuery $node object", function(){
+  it("should inherit from Dance class", function(){
     expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(blinkyDancer.setPosition).to.be.a.function;
+  });
+
+  it("should have the constructor type BlinkyDancer", function(){
+    expect(blinkyDancer.constructor).to.equal(BlinkyDancer);
   });
 
   it("should have a step function that makes its node blink", function() {
@@ -22,10 +28,11 @@ describe("blinkyDancer", function() {
   describe("dance", function(){
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
+      console.log(blinkyDancer);
+      
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-      clock.tick(timeBetweenSteps);
 
+      clock.tick(timeBetweenSteps); 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
