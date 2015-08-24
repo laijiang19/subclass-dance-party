07939dc5b5e067a46e156853185b4c8262384cac
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-24 15:35:07 -0500

    Created tests and refactored dancer class to be pseudoclassical pattern. Passed all the tests

---
+++ spec/dancerSpec.js
@@ -0,0 +1,31 @@
+describe("dancer", function() {
+
+  var dancer;
+  var clock;
+  
+  beforeEach(function() {
+    dancer = new Dancer(2, 3, 100);
+    clock = sinon.useFakeTimers();
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(dancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have functions to set step and position", function(){
+    expect(dancer.step).to.be.an.Function;
+    expect(dancer.setPosition).to.be.a.function;
+  });
+
+  it("should schedule the next step", function(){
+    sinon.spy(dancer, "step");
+    dancer.step();
+    clock.tick(dancer.timeBetweenSteps);
+    expect(dancer.step.called).to.be.true;
+  });  
+
+  it("should set position of the dancer",function(){
+    expect(dancer.$node[0].style.top).to.equal('2px');
+    expect(dancer.$node[0].style.left).to.equal('3px');
+  });
+});
