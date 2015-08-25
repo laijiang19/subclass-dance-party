220e66e34be0c172af800dea96fa2652311cb811
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-25 01:32:58 -0500

    Modified dancer and init; implemented line up function

--- src/dancer.js
+++ src/dancer.js
@@ -3,32 +3,35 @@ var Dancer = function(top, left, timeBetweenSteps){
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step();
   this.timeBetweenSteps = timeBetweenSteps;
+  this.step();
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
 };
 
-var count = 1;
 Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  console.log('----scheduling----');
-  console.log(count);
-  count++;
-  console.log(this.timeBetweenSteps);
-  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  this.timer = setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
+  var dim = Math.random() * 150 + 20;
   var styleSettings = {
+    height: dim,
+    width: 0,
+    "border-width": Math.random() * 3,
     top: top,
     left: left
   };
-  this.$node.css(styleSettings);
+  this.$node.animate(styleSettings);
+};
+
+Dancer.prototype.lineUp = function(){
+  clearTimeout(this.timer);
 };
--- src/init.js
+++ src/init.js
@@ -19,15 +19,71 @@ $(document).ready(function(){
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
+    console.log(dancerMakerFunctionName);
 
     // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(dancer);
     $('body').append(dancer.$node);
+  });
+  
+  var timer;
+
+  $(".generate").on("click", function(event){
+    var index;
+    var blink = window['BlinkyDancer'];
+    var rotate = window['RotatingDancer'];
+    var jump = window['JumpingDancer'];
+    var Con = [blink, rotate, jump];
+    var dancer;
+    var cb = function(){
+      index = Math.floor(Math.random()*3);
+      dancer = new Con[index](
+        $("body").height() * Math.random(),
+        $("body").width() * Math.random(),
+        Math.random() * 1000
+      );
+      window.dancers.push(dancer);
+      $('body').append(dancer.$node);
+    };
+    timer = setInterval(cb,10);
+  });
+
+  $(".lineUp").on("click", function(event){
+    var left = 20;
+    var top = $('body').height()/2;
+    var width = $('body').width();
+    var length = window.dancers.length;
+    var dancerHeight; 
+    var r,g,b;
+    clearInterval(timer);
+    _.each(window.dancers, function(dancer){
+      dancer.lineUp();
+      dancerHeight = dancer.$node.css('height');
+      console.log(dancerHeight);
+      r = Math.floor(Math.random()*256);
+      g = Math.floor(Math.random()*256);
+      b = 160;
+      dancer.$node.animate({
+        top: top,
+        left: left,
+      });
+      dancer.$node.css({
+        borderTopColor: "rgb("+r+","+g+","+b+")",
+        borderLeftColor: "rgb("+r+","+g+","+b+")",
+        borderRightColor: "rgb("+r+","+g+","+b+")",
+        borderBottomColor: "rgb("+r+","+g+","+b+")"
+      });
+      left += width/length;
+      // if (left > $('body').width() + 20){
+      //   top -= 50;
+      //   left = 20;
+      // }
+    });
   });
 });
 
